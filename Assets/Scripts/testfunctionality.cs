fe8efcc7c1a0c06ab5edd0ea46b3ab02244ec638
RavenFoxx <53256536+RavenFoxx@users.noreply.github.com>
2021-11-27 22:34:58 +0800

    updated WorkplaceAction assets and fixed text dialogue code

--- Assets/Scripts/testfunctionality.cs
+++ Assets/Scripts/testfunctionality.cs
@@ -2,47 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class testfunctionality : MonoBehaviour
 {
     public int MoneyEarned = 100;
     public float minimumChanceToPromote = 0.1f;
     public float currentChanceToPromote;
+    public Text textDisplay;
+    
+    private IEnumerator Working()
+    {
+        SceneManager.LoadSceneAsync("Workplace", LoadSceneMode.Additive);
+        Debug.Log("Playing Animation");
+
+        yield return new WaitForSecondsRealtime (5);
+        SceneManager.UnloadSceneAsync("Workplace");
+        Work();
+    }
+    public void GoToWork()
+    {
+        StartCoroutine(Working());      
+    }
+
     public void Work()
     {
-        SceneManager.LoadScene(1);
         PlayerData.Instance.Money += MoneyEarned;
         Debug.Log("You earned $100 from working today!");
+        textDisplay.text = "You earned $" + MoneyEarned + " from working today!";
         PlayerData.Instance.Energy -= 50;
         Debug.Log("You lost 50 energy from working.");
+        //textDisplay.GetComponent<Text>().text = " You lost" +  
         currentChanceToPromote += 0.1f;
     }
+
+    private IEnumerator Promoting()
+    {
+        SceneManager.LoadSceneAsync("PromotionScene", LoadSceneMode.Additive);
+        Debug.Log("Playing Animation");
+
+        yield return new WaitForSecondsRealtime(5);
+        SceneManager.UnloadSceneAsync("PromotionScene");
+        Promotion();
+    }
+
+    public void GoToPromote()
+    {
+        if(currentChanceToPromote < minimumChanceToPromote)
+        {
+            Debug.Log("Sorry, You do not meet the requirements!");
+            textDisplay.text = "Sorry, You do not meet the requirements!";
+        }
+        else
+        {
+            StartCoroutine(Promoting());
+        }
+        
+    } 
     public void Promotion()
     {
-        SceneManager.LoadScene(2);
-
         if(currentChanceToPromote >= minimumChanceToPromote)
         {
             if(currentChanceToPromote == 1f)
             {
                 Debug.Log("Congratulations, You have been promoted and now have higher pay!");
+                textDisplay.text = "Congratulations, You have been promoted and now have higher pay!";
                 MoneyEarned += 50;
                 currentChanceToPromote = 0.0f;
             }
             if(Random.value > currentChanceToPromote)
             {
                 Debug.Log("Congratulations, You have been promoted and now have higher pay!");
+                textDisplay.text = "Congratulations, You have been promoted and now have higher pay!";
                 MoneyEarned += 50;
                 currentChanceToPromote = 0.0f;
             }
             else
             {
                 Debug.Log("Sorry, You have failed to promote!");
+                currentChanceToPromote -= 0.05f;
+                textDisplay.text = "Sorry, You have failed to promote!";
             }
         }
-        else
-        {
-            Debug.Log("Sorry, You do not meet the requirements!");
-        }
+    }
+    public void ExitWorkplace()
+    {
+        textDisplay.text = "You leave the bank.";
+        SceneManager.UnloadSceneAsync("WorkplaceAction");
     }
 }
