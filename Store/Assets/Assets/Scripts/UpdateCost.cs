3459e4a493f4bd45be3015d0f039a516f148b9f6
mnmrn <94857862+mnmrn@users.noreply.github.com>
2021-12-08 16:09:44 +0800

    update to store code
    -text now shows each object's cost
    -fixed image on first time load in scene
    -saving of amount left of each object now works
    
    to do:
    put storeData.restockItems() function in next day function
    noodles amount left is bugged (?)

---
+++ Store/Assets/Assets/Scripts/UpdateCost.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCost : MonoBehaviour
+{
+    public Stuff stuff;
+    void Start()
+    {
+        GetComponent<Text>().text = "$" + stuff.cost;
+    }
+
+}
