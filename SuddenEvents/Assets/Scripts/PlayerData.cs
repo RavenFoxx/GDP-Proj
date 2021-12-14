b54b46a833a45f94ba66ecb9dc48f219068a61bc
njxjohn <johnngjunxin@gmail.com>
2021-12-14 14:54:00 +0800

    sudden event 1
    made changes to sudden event 1

---
+++ SuddenEvents/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static PlayerData Instance { get; private set; }
+    public float Money;
+    public int Energy;
+    public int Hunger;
+    public int Happiness;
+    public int Day;
+    public int Intelligence;
+    public int Goal;
+    public int Wage;
+    public string Name;
+    public string NText;
+    private void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Day = 1;
+            Money = 300;
+            Energy = 100;
+            Hunger = 50;
+            Happiness = 50;
+            Goal = 3000;
+            Wage = 100;
+        }
+        else Destroy(gameObject);
+    }
+}
---
+++ SuddenEvents/Assets/Scripts/SuddenEvents.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class SuddenEvents : MonoBehaviour
+{
+    PlayerData _inst = PlayerData.Instance;
+    public float Money = 100;
+    public int Energy;
+    public int Hunger;
+    public int Happiness;
+    public GameObject popUpBox;
+    public Animator animator;
+    public Text popUpText;
+    public Button yesButton;
+    public Button noButton;
+
+    public void PopUp(string text)
+    {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        animator.SetTrigger("pop");
+    }
+    private void Start()
+    {
+        if (SceneManager.GetSceneByName("SuddenEvents 1").isLoaded) {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SuddenEvents 1"));
+            popUpText.text = _inst.Name + " realised that " + _inst.Name + "'s current balance is 60 dollar short compared to the original balance.";
+        }
+        else if (SceneManager.GetSceneByName("SuddenEvents 2").isLoaded) {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SuddenEvents 2"));
+            popUpText.text = _inst.Name + "'s friend asked if " + _inst.Name + " is keen to catch up while watching a movie.";
+        }
+        else if (SceneManager.GetSceneByName("SuddenEvents 3").isLoaded) {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SuddenEvents 3"));
+            popUpText.text = _inst.Name + "'s uncle asked " + _inst.Name + " to help him pack and move his stuff into his new house.";
+        }
+        else if (SceneManager.GetSceneByName("SuddenEvents 4").isLoaded) {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SuddenEvents 4"));
+            popUpText.text = _inst.Name + "'s bag is getting old and " + _inst.Name + " wants to get a new bag.";
+        }
+        else if (SceneManager.GetSceneByName("SuddenEvents 5").isLoaded) {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SuddenEvents 5"));
+            popUpText.text = _inst.Name + "'s friends has invited " + _inst.Name + " to play basketball with them.";
+        }
+        else Debug.Log("Scene out of bounds or not found");
+    }
+    public void Scenario1Yes()
+    {
+        _inst.Energy -= 20;
+        _inst.Hunger -= 10;
+        _inst.NText = "The bank has replied and said that there's an error on their part and they have fixed it as well as given an additional 10 dollars as compensation.";
+    }
+    public void Scenario1No()
+    {
+        _inst.Money -= 60;
+        _inst.NText= "There's a hidden fee and " + _inst.Name + " got charged 60 dollars without knowing, this could be avoided by contacting the bank.";
+    }
+    public void Scenario2Yes()
+    {
+        _inst.Money -= 30;
+        _inst.Energy -= 20;
+        _inst.Hunger -= 20;
+        _inst.Happiness += 20;
+        _inst.NText= _inst.Name + " had fun for the day.";
+    }
+    public void Scenario2No()
+    {
+        _inst.Happiness -= 10;
+        _inst.NText = _inst.Name + " felt a little sad as " + _inst.Name + " decline his friend's offer to hang out.";
+    }
+    public void Scenario3Yes()
+    {
+        _inst.Money += 30;
+        _inst.Energy -= 20;
+        _inst.Hunger -= 20;
+        _inst.Happiness += 20;
+        _inst.NText = _inst.Name + " have been treated to a meal as well as received 20 dollars from his uncle.";
+    }
+    public void Scenario3No()
+    {
+        _inst.NText = _inst.Name + "'s uncle replied that he'll find someone else.";
+    }
+    public void Scenario4Yes()
+    {
+        _inst.Money -= 50;
+        _inst.Happiness += 20;
+        _inst.NText = _inst.Name + " like his new bag";
+    }
+    public void Scenario4No()
+    {
+        _inst.Money -= 30;
+        _inst.NText = _inst.Name + " have a new bag";
+    }
+    public void Scenario5Yes()
+    {
+        _inst.Energy -= 40;
+        _inst.Happiness += 30;
+        _inst.NText = _inst.Name + " enjoyed playing basketball with his friends.";
+    }
+    public void Scenario5No()
+    {
+        _inst.Happiness -= 10;
+        _inst.NText = _inst.Name + " is disheartened that he couldn't join his friends.";
+    }
+ 
+    public void closePopUp()
+    {
+        popUpBox.SetActive(false);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Main"));
+    }
+
+}
+            
+        
+
+
+
+
+
+
+  
+
+
+
+  
